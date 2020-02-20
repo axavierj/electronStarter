4169f67d118b25a96694e5753b3e958cc413cbd6
Alexander Jimenez <alexanderjimenez29@gmail.com>
2020-02-20 16:32:49 -0500

    Initial Commit

---
+++ public/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from 'react-bootstrap/Navbar'
+
+const App = () => {
+    return ( 
+        <NavBar bg="primary" variant="dark">
+            <NavBar.Brand href="#">App</NavBar.Brand>
+        </NavBar>
+     )
+}
+ 
+export default App
